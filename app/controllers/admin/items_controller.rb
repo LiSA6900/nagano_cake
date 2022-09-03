7a27fd051322a1a34fa66a2564d4db721ec3bea6
EC2 Default User <ec2-user@ip-172-31-30-161.ap-northeast-1.compute.internal>
2022-09-03 15:44:45 +0000

    [Add]商品一覧画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,23 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
+
+  def index
+    @items = Item.all
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
+  end
+
 end
