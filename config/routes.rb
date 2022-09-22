6f9e67587f51c43c63efb0e84c87243fc8fd6e9b
EC2 Default User <ec2-user@ip-172-31-30-161.ap-northeast-1.compute.internal>
2022-09-22 14:56:29 +0000

    [Add]リンクの調整

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   }
   scope module: :public do
     get "customers/my_page" => "customers#show"
-    get "customers/informationrails /edit" => "customers#edit"
+    get "customers/information/edit" => "customers#edit"
     patch "customers/information" => "customers#update"
     # 退会確認画面
     get "customers/unsubscribe" => "customers#unsubscribe"
